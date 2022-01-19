c0154020ff3c6a97ed724e3e376984b68352e7d3
Partha Roy <partharoy@Parthas-MacBook-Air.local>
2022-01-19 16:42:24 +0530

    Added bootstrap + localstorage example

--- src/components/Loop.js
+++ src/components/Loop.js
@@ -1,6 +1,6 @@
 const Loop = function () {
   const loopArray = ["Piyanshee", "Vishwanath", "Tanisha", "Vedanshee"];
-
+  const nameSaved = localStorage.getItem("name");
   return (
     <div>
       Loop
@@ -9,6 +9,30 @@ const Loop = function () {
           return <li key={index}>{item}</li>;
         })}
       </ul>
+      <div className="container">
+        <form>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">
+              Name ( store in localstorage )
+            </label>
+            <input
+              type="text"
+              defaultValue={nameSaved}
+              onChange={(e) => {
+                if (window && localStorage) {
+                  localStorage.setItem("name", e.target.value);
+                }
+              }}
+              class="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+            <div id="emailHelp" class="form-text">
+              We'll never share your email with anyone else.
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
